c57d0180405e8e6ae1ae2a18cc8cd04635accac6
andreydro <andreydro14@gmail.com>
2018-02-01 22:22:22 +0300

    Update animal classes

--- animals.rb
+++ animals.rb
@@ -1,47 +1,99 @@
 class Bird
-  def talk(name)
-    puts "#{name} says Chirp! Chirp!"
+
+  attr_reader :name, :age
+
+  def name=(value)
+   if value == ""
+   	 raise "Name can't be blank!"
+   end
+   @name = value
   end
 
-  def move(name, destination)
-    puts "#{name} flys to the #{destination}"
+  def age=(value)
+  	if value < 0
+  	  raise "An age of #{value} isn`t valid!"
+  	end
+  	@age = value
+  end
+
+  def report_age
+  	puts "#{@name} is #{@age} years old."
+  end
+
+  def talk
+    puts "#{@name} says Chirp! Chirp!"
+  end
+
+  def move(destination)
+    puts "#{@name} flys to the #{destination}"
   end
 end
 
 class Dog
-  def make_up_name
-    @name = "Sandy"
+  
+  attr_reader :name, :age
+
+  def name=(value)
+   if value == ""
+   	 raise "Name can't be blank!"
+   end
+   @name = value
   end
 
-  def talk(name)
-    puts "#{@name} says Bark!"
+  def age=(value)
+  	if value < 0
+  	  raise "An age of #{value} isn`t valid!"
+  	end
+  	@age = value
+  end
+
+  def report_age
+  	puts "#{@name} is #{@age} years old."
+  end
+
+  def talk
+  	puts "#{@name} says Bark!"
+  end
+
+  def move(destination)
+  	puts "#{@name} runs to the #{destination}"
+  end
+end
+
+class Cat
+
+  attr_reader :name, :age
+
+  def name=(value)
+   if value == ""
+   	 raise "Name can't be blank!"
+   end
+   @name = value
+  end
+
+  def age=(value)
+  	if value < 0
+  	  raise "An age of #{value} isn`t valid!"
+  	end
+  	@age = value
+  end
+
+  def report_age
+  	puts "#{@name} is #{@age} years old."
+  end
+
+  def talk
+    puts "#{@name} says Meow!"
   end
 
   def move(destination)
     puts "#{@name} runs to the #{destination}"
   end
-
-  def make_up_age
-  	@age = 5
-  end
-
-  def report_age
-  	puts "#{@name} is #{@age} years old"
-  end
-end
-
-class Cat
-  def talk(name)
-    puts "#{name} says Meow!"
-  end
-
-  def move(name, destination)
-    puts "#{name} runs to the #{destination}"
-  end
 end
 
 dog = Dog.new
-dog.make_up_name
-dog.move("yard")
-dog.make_up_age
+dog.name = "Daisy"
+dog.age = 3
 dog.report_age
+dog.talk
+dog.move("bed")
