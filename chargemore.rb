fa358c323cdf1df34655705f51a57edd679cbf67
andreydro <andreydro14@gmail.com>
2018-02-06 22:54:09 +0300

    Update classes in chargemore.rb

--- chargemore.rb
+++ chargemore.rb
@@ -37,7 +37,7 @@ class SalariedEmployee < Employee
 
   def print_pay_stub
     print_name
-    pay_for_period = (@salary / 365.0) * 14
+    pay_for_period = (salary / 365.0) * 14
     formatted_pay = format("$%.2f", pay_for_period)
     puts "Pay This Period: #{formatted_pay}"
   end
@@ -45,19 +45,31 @@ class SalariedEmployee < Employee
 end
 
 class HourlyEmployee < Employee
+
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+
+  def self.cashier(name)
+    HourlyEmployee.new(name,12.75, 25)
+  end
+
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.50, 20)
+  end
   
   attr_reader :hourly_wage, :hours_per_week
 
   def hourly_wage=(hourly_wage)
     if hourly_wage < 0
-      raise "A wage oer hour of #{hourly_wage} isn't valid!"
+      raise "An hourly wage of #{hourly_wage} isn't valid!"
     end
     @hourly_wage = hourly_wage 
   end
 
   def hours_per_week=(hours_per_week)
     if hours_per_week < 0
-      raise "Hours per week of #{hours_per_week} isn't valid!"
+      raise "#{hours_per_week} hours per week isn't valid!"
     end
     @hours_per_week = hours_per_week
   end
@@ -72,13 +84,15 @@ class HourlyEmployee < Employee
     print_name
     pay_for_period = hourly_wage * hours_per_week * 2
     formatted_pay = format("$%.2f", pay_for_period)
-    puts "Pay Whis Period #{formatted_pay}" 
+    puts "Pay This Period #{formatted_pay}" 
   end
 
 end
 
-#salaried_employee = SalariedEmployee.new("Jane Doe", 50000)
-#salaried_employee.print_pay_stub
+jane = SalariedEmployee.new("Jane Doe", 50000)
+jane.print_pay_stub
 
-hourly_employee = HourlyEmployee.new("John Smith", 14.97, 30)
-hourly_employee.print_pay_stub
+angela = HourlyEmployee.security_guard("Angela Matthews")
+ivan = HourlyEmployee.cashier("Ivan Stokes")
+angela.print_pay_stub
+ivan.print_pay_stub
