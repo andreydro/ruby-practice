59fe070abda643369c890c24c75e2395b24ad0ed
andreydro <andreydro14@gmail.com>
2018-02-22 18:55:37 +0300

    Add-stars.rb

---
+++ stars.rb
@@ -0,0 +1,35 @@
+class CelestialBody
+  attr_accessor :type, :name
+end
+
+altair = CelestialBody.new
+altair.name = 'Altair'
+altair.type = 'star'
+polaris = altair
+polaris.name = 'Polaris'
+vega = polaris
+vega.name = 'Vega'
+
+#puts altair.name, polaris.name, vega.name
+
+class LoveInterest
+  
+  def request_date
+    if @busy
+      puts "Sorry, I'm busy."
+    else
+  	  puts "Sure, let's go!"
+  	  @busy = true
+    end
+  end
+
+end
+
+betty = LoveInterest.new
+candace = betty
+
+p betty.object_id
+p candace.object_id
+
+betty.request_date
+candace.request_date
