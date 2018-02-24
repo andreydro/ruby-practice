966688d50c73629a4a66b3a1438e98d261c20e5b
andreydro <andreydro14@gmail.com>
2018-02-24 11:22:42 +0300

    Update stars.rb

--- stars.rb
+++ stars.rb
@@ -2,32 +2,50 @@ class CelestialBody
   attr_accessor :type, :name
 end
 
-altair = CelestialBody.new
-altair.name = 'Altair'
-altair.type = 'star'
-polaris = altair
-polaris.name = 'Polaris'
-vega = polaris
-vega.name = 'Vega'
+bodies = Hash.new { |hash, key|
+  body = CelestialBody.new
+  body.type = "planet"
+  hash[key] = body 
+}
+
+bodies['Mars'].name = 'Mars'
+bodies['Europe'].name = 'Europe'
+bodies['Europe'].type = 'moon'
+bodies['Venus'].name = 'Venus'
+
+p bodies
+
+
+#altair = Cele
+#stialBody.new
+#altair.name = 'Altair'
+#altair.type = 'star'
+#polaris = CelestialBody.new
+#polaris.name = 'Polaris'
+#polaris.type = 'star'
+#vega = CelestialBody.new
+#vega.name = 'Vega'
+#vega.type = 'star'
+
 #puts altair.name, polaris.name, vega.name
-class LoveInterest
-  
-  def request_date
-    if @busy
-      puts "Sorry, I'm busy."
-    else
-  	  puts "Sure, let's go!"
-  	  @busy = true
-    end
-  end
+#class LoveInterest
+#  
+#  def request_date
+#    if @busy
+#      puts "Sorry, I'm busy."
+#    else
+#  	  puts "Sure, let's go!"
+#  	  @busy = true
+#    end
+#  end
 
-end
+#end
 
-betty = LoveInterest.new
-candace = betty
+#betty = LoveInterest.new
+#candace = betty
 
-p betty.object_id
-p candace.object_id
+#p betty.object_id
+#p candace.object_id
 
-betty.request_date
-candace.request_date
+#betty.request_date
+#candace.request_date
