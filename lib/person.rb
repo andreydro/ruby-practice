b9f81753bdf542f964e19234ac4b2a8f5af3976d
andreydro <andreydro14@gmail.com>
2018-02-28 18:53:57 +0300

    Work-with-minitest

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,6 @@
 class Person
   attr_accessor :name
   def introduction
-    person = Person.new
-    person.name = 'Bob'
-    puts person.introduction == 'Hello, my name is Bob!'
+    'Hello, my name is Bob!'
   end
 end
