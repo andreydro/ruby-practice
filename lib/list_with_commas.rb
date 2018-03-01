2684d66886f3222bc4f129e89421538388b27116
andreydro <andreydro14@gmail.com>
2018-03-01 19:50:53 +0300

    Finish-work-with-minitest

--- lib/list_with_commas.rb
+++ lib/list_with_commas.rb
@@ -1,7 +1,9 @@
 class ListWithCommas
   attr_accessor :items
   def join
-  	if items.length == 2
+  	if items.length == 1
+  	  return items[0]
+  	elsif items.length == 2
   	  return "#{items[0]} and #{items[1]}"
   	end
 	last_item = "and #{items.last}"
@@ -9,3 +11,17 @@ class ListWithCommas
 	"#{other_items}, #{last_item}"
   end
 end
+
+=begin
+two_subjects = ListWithCommas.new
+two_subjects.items = ['my parents','a rodeo clown']
+puts "A photo of #{two_subjects.join}"
+three_subjects = ListWithCommas.new
+three_subjects.items = ['my parents','a rodeo clown', 'a prize bull']
+puts "A photo of #{three_subjects.join}"
+
+
+one_subject = ListWithCommas.new
+one_subject.items = ['a rodeo clown']
+puts "A photo of #{one_subject.join}"
+=end
