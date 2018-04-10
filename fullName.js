5e692de855577b71b767356ac49e6521f7f1b532
andreydro <andreydro14@gmail.com>
2018-04-11 00:34:44 +0300

    Add-fullName-function

---
+++ fullName.js
@@ -0,0 +1,31 @@
+function User(fullName) {
+	this.fullName = fullName;
+
+	Object.defineProperties(this, {
+		firstName: {
+			get: function() {
+				return this.fullName.split(" ")[0];
+			},
+			set: function(newFirstName) {
+				this.fullName = newFirstName + " " + this.lastName;
+			}
+		},
+		lastName: {
+			get: function() {
+				return this.fullName.split(" ")[1];
+			},
+			set: function(newLastName) {
+				this.fullName = this.firstName + " " + newLastName;
+			}
+		}
+	});
+}
+
+var vasya = new User("Vasya Popkin");
+
+console.log(vasya.firstName);
+console.log(vasya.lastName);
+
+vasya.lastName = "Sidorov";
+
+console.log(vasya.fullName);
