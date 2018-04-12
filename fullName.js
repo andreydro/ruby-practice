68f9a78ec36e8500394aa4efe111f8421dab9af8
andreydro <andreydro14@gmail.com>
2018-04-12 23:49:32 +0300

    Add-sandwich-koan

--- fullName.js
+++ fullName.js
@@ -1,24 +1,24 @@
 function User(fullName) {
-	this.fullName = fullName;
+    this.fullName = fullName;
 
-	Object.defineProperties(this, {
-		firstName: {
-			get: function() {
-				return this.fullName.split(" ")[0];
-			},
-			set: function(newFirstName) {
-				this.fullName = newFirstName + " " + this.lastName;
-			}
-		},
-		lastName: {
-			get: function() {
-				return this.fullName.split(" ")[1];
-			},
-			set: function(newLastName) {
-				this.fullName = this.firstName + " " + newLastName;
-			}
-		}
-	});
+    Object.defineProperties(this, {
+        firstName: {
+            get: function() {
+                return this.fullName.split(" ")[0];
+            },
+            set: function(newFirstName) {
+                this.fullName = newFirstName + " " + this.lastName;
+            }
+        },
+        lastName: {
+            get: function() {
+                return this.fullName.split(" ")[1];
+            },
+            set: function(newLastName) {
+                this.fullName = this.firstName + " " + newLastName;
+            }
+        }
+    });
 }
 
 var vasya = new User("Vasya Popkin");
