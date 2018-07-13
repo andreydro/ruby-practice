a782e3227cb28dc094dec2e4d0c4bc55749da93f
andreydro <andreydro14@gmail.com>
2018-07-13 14:10:11 +0300

    Finish level 1 in versus app

--- versus/versus.rb
+++ versus/versus.rb
@@ -1,40 +1,15 @@
-# require 'rubygems'
-# require 'commander/import'
+require 'terminal-table'
+require 'fuzzy_match'
+require 'optparse'
 
-# # :name is optional, otherwise uses the basename of this executable
-# program :name, 'versus'
-# program :version, '1.0.0'
-# program :description, 'You will see the versus statistics'
+SWEAR_WORDS_ARRAY = [
+  "блять","блядь","бляди","ебать","ёб","бля","блядина","блядский","блядистость","блядогон","блядословник",
+  "блядский","блядство","въебаться","взбляд","впиздячил","выблядовал","выблядок","выебон","выёбывается",
+  "глупизди","доебался","ебанёшься","ебанул","ебашит","ёбнул","ебало","ебанулся","ебнул","жидоёб","жидоёбский",
+  "заёб","изъебнулся","заебал","заебись","козлоёб","козлоёбище","хуй","пизда","пиздец",'\*',"сука"
+]
 
-# global_option '--top-bad-words'
-
-# command :go do |c|
-#   c.syntax = 'run the command'
-#   c.description = 'lets test'
-#   c.action do |args, options|
-#     say "yes, you are right" if options.test
-#   end
-# end
-
-#NAMES = %["(Pra(Killa'Gramm)", 'Obe_1_Kanobe', '13|47', 'Леха_Медь', '1917', 'Palmdropov', 'Abbalbisk', 'A']
-DRAGO = 'Drago'
-
-ABBA = 'Abbalbisk'
-
-SWEAR_WORDS = 'сука|блять|блядь|ебать|бля|блядина|блядский|ебало|ебанулся|ебнул|заебал|заебись|хуй|пизда|пиздец' # дополнить
-
-# Dir.glob("rap-battles/*") do |file_name|
-#   battle = file_name.match(/Drago/)
-#   battles_storage << battle
-#   battles_storage.reject! { |c| c.nil? }
-#   text = File.read(file_name)
-#   find_name = text.match(/Drago/)
-#   names_storage << find_name
-#   names_storage.reject! { |c| c.nil? }
-# end
-
-
-# puts names_storage.size
+SWEAR_WORDS = SWEAR_WORDS_ARRAY.join('|')
 
 def find_all_rapers
   raper_storage = []
@@ -70,13 +45,15 @@ def number_of_swear_words(name)
   Dir.glob("rap-battles/*") do |file_name|
     if file_name.match(name)
       text = File.read(file_name)
-      find_swear_words = text.match(SWEAR_WORDS)
+      find_swear_words = text.scan(/#{SWEAR_WORDS}/)
       swear_words_storage << find_swear_words
-      swear_words_storage.reject! { |c| c.nil? }
+      swear_words_storage.flatten!
     end
   end
   swear_words_storage.size
 end
+
+#print number_of_swear_words(ATL)
 
 def average_number_of_swearing_words_in_battle(name)
   average = number_of_swear_words(name).fdiv(number_of_battles(name))
@@ -105,30 +82,58 @@ def average_number_words_in_round(name)
   number_of_words_in_rounds / number_of_rounds
 end
 
-def statistics
+def statistics(number)
   all_rapers = find_all_rapers
   storage_hash = {}
+  final_storage = {}
   all_rapers.each do |raper|
     storage_for_each_raper = []
-    battles = number_of_battles(raper)
-    swear_words = number_of_swear_words(raper)
-    avn_swear_words = average_number_of_swearing_words_in_battle(raper)
-    avn_words_in_round = average_number_words_in_round(raper)
-    storage_for_each_raper << battles
-    storage_for_each_raper << swear_words
-    storage_for_each_raper << avn_swear_words
-    storage_for_each_raper << avn_words_in_round
+    storage_for_each_raper << number_of_battles(raper)
+    storage_for_each_raper << number_of_swear_words(raper)
+    storage_for_each_raper << average_number_of_swearing_words_in_battle(raper)
+    storage_for_each_raper << average_number_words_in_round(raper)
     storage_hash[raper] = storage_for_each_raper
   end
-  storage_hash
+  storage_hash = storage_hash.sort_by { |key, value| value[1] }
+  storage_hash.reverse!
+  storage_hash.each do |elem|
+    final_storage[elem[0]] = elem[1]
+  end
+  final_storage.first(number.to_i)
 end
 
-puts statistics
+#print statistics.first(3)
 
-# puts "Swearing words " + number_of_swear_words(ABBA).to_s
-# puts "Battles " + number_of_battles(ABBA).to_s
-# puts "Average SW " + average_number_of_swearing_words_in_battle(ABBA).to_s
-# puts "-----"
-# puts "Swearing words " + number_of_swear_words(DRAGO).to_s
-# puts "Battles " + number_of_battles(DRAGO).to_s
-# puts "Average SW " + average_number_of_swearing_words_in_battle(DRAGO).
+def make_table(number)
+  all_data = statistics(number)
+  filtered_data = {}
+  all_data.each do |elem|
+    filtered_data[elem[0]] = elem[1]
+  end
+  rows = []
+  filtered_data.each do |key, value|
+    rows << [key.to_s, value[0].to_s + ' батлов', value[1].to_s + ' нецензурных слов', value[2].to_s + ' слова на батл', value[3].to_s + ' слова в раунде']
+  end
+  table = Terminal::Table.new :rows => rows
+  table
+end
+
+#print make_table(10)
+
+options = {"top-bad-words" => nil}
+
+parser = OptionParser.new do|opts|
+  opts.banner = "Info for all options"
+  opts.on('-bad-words', '--top-bad-words=number', 'Yeap, test passed') do |number|
+    options["top-bad-words"] = number;
+  end
+end
+
+parser.parse!
+
+if options["top-bad-words"] == nil
+  print 'You should enter number of rapers you want to see: '
+    options["top-bad-words"] = gets.chomp
+end
+
+print make_table(options["top-bad-words"])
